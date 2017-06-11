1b960fa632aaef0cab7f3a75e2788d8e03bdf333
latisdorsi <chrohn@outlook.com>
2017-06-11 14:29:47 +0800

    Added Ajax to Login page to make POST request and save cookies

--- web-client/js/script.js
+++ web-client/js/script.js
@@ -6,10 +6,52 @@
   }
   );
 
-	
-  $(document).ready(function(){
+
+	$(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
-  });
-          
+
+    //Login Button Function
+    $('#btn-login').click(function(){
+    	$.ajax({
+    		type: "POST",
+    		url: "/ws/account/login",
+   			 // The key needs to match your method's input parameter (case-sensitive).
+
+		    data: JSON.stringify({ "username": $('#username').val(), "password":$('#password').val()}),
+		    crossDomain: true,
+		    success: function(data){
+          $.cookie("session", data.session);
+          alert($.cookie("session"));
+        },
+
+		    failure: function(errMsg) {
+		    	alert(errMsg);
+		    },
+		    dataType: "json",
+		    contentType: "application/json"
+		});
+      $('#btn-register').click(function(){
+      $.ajax({
+        type: "POST",
+        url: "/ws/account/create/customer",
+         // The key needs to match your method's input parameter (case-sensitive).
+
+        data: JSON.stringify({ "username": $('#username').val(), "password":$('#password').val(), "Name": $("#name").val(), "type" : "NORMAL"}),
+        crossDomain: true,
+        success: function(data){
+          alert('successfully registered!')
+          $('#modal1').modal('close');
+        
+        },
+
+        failure: function(errMsg) {
+          alert(errMsg);
+        },
+        dataType: "json",
+        contentType: "application/json"
+    });
+    });
+});
+
 
